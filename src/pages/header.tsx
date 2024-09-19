10eec84515736827b8c9b7e9616c0ccc4592e00d
FelipeLedur <felps5000@gmail.com>
2024-09-18 22:24:07 -0300

    Adição dos card + correção bugs e melhorias

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Logo from '../assets/aaaaa.svg';
 import Close from '../assets/Close.svg';
 import Menu from '../assets/Header.svg';
+import Award from '../assets/award.svg';
 import '../styles/header.css';
 import '../styles/utility.css';
 import Button from '../components/Button';
@@ -87,10 +88,10 @@ export default function Home() {
                 <img src={HeroRectangleOne} alt="Retangulo dois tela inicial" />
                 <div className="container content">
                     <p className="desktop-only">
-                        Olá
+                        <br /><br /><br /><br /><br />
                     </p>
                     <h1>Com o Seu Silo você nunca mais ficará sem ração!</h1>
-                    <p>Já pensou em pedir ração diretamente pelo seu celular? Apenas o Seu Silo faz isso para você!
+                    <p >Já pensou em pedir ração diretamente pelo seu celular? Apenas o Seu Silo faz isso para você!
                     </p>
                     <div className="flex gap-1">
                         <span><Button text="Cadastre-se" /></span>
@@ -98,7 +99,48 @@ export default function Home() {
                             <Button text="Veja mais" secondary />
                         </span>
                     </div>
+                    <h1>
+                        Sob Medida para você!
+                    </h1>
+                    <p>
+                        O Seu silo é a solução que você precisava para sua fazenda, venha ser mais um de nossos muitos clientes.
+                    </p>
+                    <p>
+                        Inovação é com a gente! Veja tudo que pode ganhar com nossos serviços
+                    </p>
+
+                    <div className="cards-container">
+
+
+
+                        <div className="card">
+                            <div className="card-icon">
+                                <img src={Award} alt="ícone menu" width={24} height={24} />
+                            </div>
+                            <h3>Produto de Qualidade</h3>
+                            <p className="desktop-only">Nosso produto já recebou várias premiações na questão de qualidade e funcionamento</p>
+                        </div>
+
+                        <div className="card">
+                            <div className="card-icon">
+                                <img src={Award} alt="ícone menu" width={24} height={24} />
+                            </div>
+                            <h3>Produto Único</h3>
+                            <p className="desktop-only">Um produto nunca antes visto no mercado, 100% inovativo</p>
+                        </div>
+
+                        <div className="card">
+                            <div className="card-icon">
+                                <img src={Award} alt="ícone menu" width={24} height={24} />
+                            </div>
+                            <h3>Produto Recomendado</h3>
+                            <p className="desktop-only">App com 4.9 estrelas na Google Play e App Store, com milhares de recomendações positivas</p>
+                        </div>
+
+                    </div>
                 </div>
+
+
             </section>
         </>
     );
