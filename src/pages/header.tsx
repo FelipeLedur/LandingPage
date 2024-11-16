e2f0306af47e7bc841283474ed0c9c899c23c6ca
FelipeLedur <felps5000@gmail.com>
2024-11-16 16:54:26 -0300

    Versao Final - 2nd Bim

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -5,13 +5,62 @@ import Menu from '../assets/Header.svg';
 import Award from '../assets/award.svg';
 import '../styles/header.css';
 import '../styles/utility.css';
+import '../styles/card.css';
+import ProfileImageOne from "../assets/ProfileImageOne.jpg"
+import ProfileImageTwo from "../assets/ProfileImageTwo.jpg"
+import ProfileImageThree from "../assets/ProfileImageThree.jpg"
 import Button from '../components/Button';
+import CardPrice from '../components/CardPrice';
 import HeroRectangleOne from "../assets/images/rectangleOne.png";
 import HeroRectangleTwo from "../assets/images/rectangleTwo.png";
+import CardCarousel from "../components/CardCarousel"
 import "../styles/hero.css";
+import "../styles/testimonials.css"
+import "../styles/pricing.css"
+import "../styles/contact.css"
+import Check from "../assets/Check.svg"
+
+
 
 export default function Home() {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
+    // armazenar os valores dos inputs
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    // coleta os dados do formulário e envia para a funcao do Google Cloud
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => { 
+        e.preventDefault();
+
+        const formData = {
+            email: email,
+            subject: subject,
+            message: message
+        };
+
+        body: JSON.stringify(formData)
+        
+        try {
+            const response = await fetch('api', {  
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjFkYzBmMTcyZThkNmVmMzgyZDZkM2EyMzFmNmMxOTdkZDY4Y2U1ZWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEzMDI5MTU3OTk2OTkxNTQ3OTc4IiwiaGQiOiJtaW5oYS5mYWcuZWR1LmJyIiwiZW1haWwiOiJmYWxlZHVyQG1pbmhhLmZhZy5lZHUuYnIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IlY5b0FJU21aR1hYVXJjT1VISWtPU3ciLCJpYXQiOjE3MzE3ODMwMzMsImV4cCI6MTczMTc4NjYzM30.QT0fqPBTMiEyL7Jyk2bfD_AX2hrnHY8HBrQ8PmWlNkw4GPA4wmNmiG2LsXYjqWAPzm7_A42b8-huSG_owC5CW1mHkld-VRXVl5jEoRzGnrPy5I-JUhUpZkozno3ifvZ43F5RLBlrF-SeNIYp9MwWtDq_9Qc-Lz6XfCZoSzyKwbG0VVROa-9tpg5mGKewk2BVUvOn7t1rVFwV-YiVINjKhvI7O-TJ_yqy63mEPPj3YGzwzOA9jWvV54cX-20IyopuFNw5ttrZrhmOXpyWLnbXz65yKHy3uwAK35R-MfGeYVfP7FKJwdZkkT0pVfeJT8dMWGqDZ2DLeJE1rTGeCdjQaQ',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                alert('E-mail enviado com sucesso!');
+            } else {
+                alert('Falha ao enviar o e-mail.');
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            alert('Erro ao enviar o e-mail.');
+        }
+    };
     return (
         <>
             <header className="container py-sm">
@@ -118,7 +167,7 @@ export default function Home() {
                                 <img src={Award} alt="ícone menu" width={24} height={24} />
                             </div>
                             <h3>Produto de Qualidade</h3>
-                            <p className="desktop-only">Nosso produto já recebou várias premiações na questão de qualidade e funcionamento.</p>
+                            <p>Nosso produto já recebou várias premiações na questão de qualidade e funcionamento.</p>
                         </div>
 
                         <div className="card">
@@ -126,7 +175,7 @@ export default function Home() {
                                 <img src={Award} alt="ícone menu" width={24} height={24} />
                             </div>
                             <h3>Produto Único</h3>
-                            <p className="desktop-only">Um produto nunca antes visto no mercado, 100% inovativo.</p>
+                            <p>Um produto nunca antes visto no mercado, 100% inovativo.</p>
                         </div>
 
                         <div className="card">
@@ -134,13 +183,170 @@ export default function Home() {
                                 <img src={Award} alt="ícone menu" width={24} height={24} />
                             </div>
                             <h3>Produto Recomendado</h3>
-                            <p className="desktop-only">App com 4.9 estrelas na Google Play e App Store, com milhares de recomendações positivas.</p>
+                            <p>App com 4.9 estrelas na Google Play e App Store, com milhares de recomendações positivas.</p>
                         </div>
 
                     </div>
                 </div>
 
 
+            </section>
+
+            <section id="testimonials">
+                <header>
+                    <span>
+                        <p className="desktop-only bold">
+                            Conselho de quem conhece
+                        </p>
+                        <h2>Cada cliente importa!</h2>
+                    </span>
+                    <p>
+                    Quem já contratou sabe da qualidade do nosso serviço, estamos tirando aquela ideia de que 
+                    o agricultor não possui acesso a tecnologia, acompanhe abaixo os testemunhos de quem já comprou e aprovou.
+                    </p>
+                </header>
+
+                <section className="carousel">
+                    <div className="carousel-content">
+                        <CardCarousel
+                            comentary="Meus silos nunca foram tão tecnológicos, agora consigo controlar tudo pelo celular."
+                            urlProfileImage={ProfileImageOne}
+                            starCloseNumber= {5}
+                            nameCostumer="Donald Trump"
+                            ocupationCostumer="Próximo presidente dos Estados Unidos"
+                        />
+                        <CardCarousel
+                            comentary="Minhas aves nunca mais ficaram sem ração, o Seu Silo é a solução que eu precisava."
+                            urlProfileImage={ProfileImageTwo}
+                            starCloseNumber= {4}
+                            nameCostumer="Biden"
+                            ocupationCostumer="Atual presidente dos Estados Unidos"
+                        />
+                        <CardCarousel
+                            comentary="Agora consigo controlar tudo pelo celular, nunca mais fico sem ração."
+                            urlProfileImage={ProfileImageThree}
+                            starCloseNumber= {4}
+                            nameCostumer="Kamala Harris"
+                            ocupationCostumer="Atual vice presidente dos Estados Unidos"
+                        />
+                    </div>
+                    <div className="carousel-content">
+                    <CardCarousel
+                            comentary="Meus silos nunca foram tão tecnológicos, agora consigo controlar tudo pelo celular."
+                            urlProfileImage={ProfileImageOne}
+                            starCloseNumber= {5}
+                            nameCostumer="Donald Trump"
+                            ocupationCostumer="Próximo presidente dos Estados Unidos"
+                        />
+                        <CardCarousel
+                            comentary="Minhas aves nunca mais ficaram sem ração, o Seu Silo é a solução que eu precisava."
+                            urlProfileImage={ProfileImageTwo}
+                            starCloseNumber= {4}
+                            nameCostumer="Biden"
+                            ocupationCostumer="Atual presidente dos Estados Unidos"
+                        />
+                        <CardCarousel
+                            comentary="Agora consigo controlar tudo pelo celular, nunca mais fico sem ração."
+                            urlProfileImage={ProfileImageThree}
+                            starCloseNumber= {4}
+                            nameCostumer="Kamala Harris"
+                            ocupationCostumer="Atual vice presidente dos Estados Unidos"
+                        />
+                    </div>
+                </section>
+            </section>
+
+            <section id="pricing" className="container">
+                <header>
+                    <p className="desktop-only">Planos e preços</p>
+                    <h2>Nossos Planos</h2>
+                </header>
+                <section className="even-columns gap-1.5">
+                <CardPrice
+                        plan="Básico"
+                        description="Baixe o ferramenta e comece a utilizar agora mesmo!"
+                        price="Grátis"
+                        benefits={[
+                            { description: "Com anúncios", urlIcon: Check },
+                            { description: "Até 10 produtos por dia", urlIcon: Check },
+                            { description: "Utilize sem limitação X", urlIcon: Check }
+                        ]}
+                    />
+
+                    <CardPrice
+                        plan="Premium"
+                        description="Para quem deseja utilizar nossa ferramenta sem limitações!"
+                        price="R$ 19,90"
+                        method="/mês"
+                        benefits={[
+                            { description: "Sem interrupção de anúncios", urlIcon: Check },
+                            { description: "Utilize quantas vezes quiser", urlIcon: Check },
+                            { description: "Utilize todos os produtos disponíveis na plataforma", urlIcon: Check }
+                        ]}
+                        premium
+                    />
+
+                    <CardPrice
+                        plan="Empresarial"
+                        description="Utilize nossa soiução na sua empresa. Aprimore seu fluxo."
+                        price="R$ 12,90"
+                        method="/mês por dev"
+                        benefits={[
+                            { description: "Com anúncios", urlIcon: Check },
+                            { description: "Até 10 produtos por dia", urlIcon: Check },
+                            { description: "Utilize sem limitação X", urlIcon: Check }
+                        ]}
+                    />
+
+                </section>
+            </section>
+
+            <section id="contact" className="container">
+                <header>
+                    <p className="desktop-only">Contato</p>
+                    <h2>Entre em contato conosco!</h2>
+                </header>
+                <section className="even-columns gap-1.5">
+                    <form onSubmit={handleSubmit} className="contact-form">
+                        <div className="form-group">
+                            <label htmlFor="email">E-mail: </label>
+                            <input 
+                                type="email" 
+                                id="email" 
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)} 
+                                required
+                                placeholder="Seu e-mail"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="subject">Assunto: </label>
+                            <input 
+                                type="text" 
+                                id="subject" 
+                                value={subject} 
+                                onChange={(e) => setSubject(e.target.value)} 
+                                required
+                                placeholder="Assunto"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="message">Mensagem: </label>
+                            <input 
+                                type="text" 
+                                id="message" 
+                                value={message} 
+                                onChange={(e) => setMessage(e.target.value)} 
+                                required
+                                placeholder="Mensagem"
+                            />
+                        </div>
+
+                        <Button text="Enviar" />
+                    </form>
+                </section>
             </section>
         </>
     );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://us-central1-iron-handler-440022-v7.cloudfunctions.net/function-Trabalho/sendEmail',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
